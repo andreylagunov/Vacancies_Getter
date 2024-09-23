cd951a66a6e70c8913ea99f31473dab98eea1a7e
Андрей Лагунов <tobeplayed@mail.ru>
2024-09-23 20:31:01 +0800

    Форматирование, типизация, документирование модуля file.py

--- src/file.py
+++ src/file.py
@@ -1,14 +1,15 @@
-from abc import ABC, abstractmethod
-from src.vacancy import Vacancy
 import json
 import os
+from abc import ABC, abstractmethod
+
+from src.vacancy import Vacancy
 
 
 class FileHandler(ABC):
-    """ Абстрактный класс для класса работы с файлами (JSON). """
+    """Абстрактный класс для класса работы с файлами (JSON)."""
 
     @abstractmethod
-    def __init__(self, file_name: str) -> None:
+    def __init__(self, file_name: str):
         """
         В экземпляре есть атрибут - имя файла,
         которое может быть назначено при создании экземпляра.
@@ -16,8 +17,8 @@ class FileHandler(ABC):
         pass
 
     @abstractmethod
-    def get_data_from_file(self):
-        """ Метод получения данных из файла. """
+    def get_data_from_file(self) -> list[dict]:
+        """Метод получения данных из файла."""
         pass
 
     @abstractmethod
@@ -30,17 +31,17 @@ class FileHandler(ABC):
 
     @abstractmethod
     def delete_data_from_file(self, vacancy: Vacancy) -> None:
-        """ Метод удаления данных из файла. """
+        """Метод удаления данных из файла."""
         pass
 
 
 class JSONHandler(FileHandler):
-    """ Класс для работы с JSON-файлами. """
+    """Класс для работы с JSON-файлами."""
 
     __file_name: str
     __relative_file_path: str
 
-    def __init__(self, file_name: str = "saved_vacancies.json") -> None:
+    def __init__(self, file_name: str = "saved_vacancies.json"):
         """
         В экземпляре есть атрибут - имя файла,
         которое может быть назначено при создании экземпляра.
@@ -51,44 +52,36 @@ class JSONHandler(FileHandler):
         else:
             self.__relative_file_path = f"./data/{file_name}"
 
-
     def get_dict_of_attributes(self) -> dict[str, str]:
-        """ Возвращает словарь со значениями атрибутов экземпляра. """
+        """Возвращает словарь со значениями атрибутов экземпляра."""
 
-        return {
-            "file_name": self.__file_name,
-            "relative_file_path": self.__relative_file_path
-        }
+        return {"file_name": self.__file_name, "relative_file_path": self.__relative_file_path}
 
     def __create_file_if_needed(self) -> None:
-        """ Создание файла для хранения вакансий, если его не существует """
+        """Создание файла для хранения вакансий, если его не существует"""
 
         if not os.path.exists(self.__relative_file_path):
             with open(self.__relative_file_path, "w", encoding="utf-8") as file:
                 json.dump([], file)
 
-
-    def __read_file(self) -> list:
-        """ Чтение json-файла. Возврат списка словарей с вакансиями. """
+    def __read_file(self) -> list[dict]:
+        """Чтение json-файла. Возврат списка словарей с вакансиями."""
 
         with open(self.__relative_file_path, "r", encoding="utf-8") as file:
             vacancy_list: list = json.load(file)
         return vacancy_list
 
-
     def __write_file(self, vacancy_list: list) -> None:
-        """ Запись в json-файл списка вакансий. """
+        """Запись в json-файл списка вакансий."""
 
         with open(self.__relative_file_path, "w", encoding="utf-8") as file:
             json.dump(vacancy_list, file, ensure_ascii=False)
 
-
     def get_data_from_file(self) -> list[dict]:
-        """ Возвращает список данных (вакансий) из файла. """
+        """Возвращает список данных (вакансий) из файла."""
 
         self.__create_file_if_needed()
         return self.__read_file()
-
 
     def add_data_to_file(self, vacancy: Vacancy) -> None:
         """
@@ -104,32 +97,29 @@ class JSONHandler(FileHandler):
                 return
 
         # Если дублирования не обнаружено, добавляем вакансию.
-        vacancy_list.append({
-            "speciality": vacancy.speciality,
-            "https_path": vacancy.https_path,
-            "salary_str": vacancy.salary_str,
-            "requirements": vacancy.requirements
-        })
+        vacancy_list.append(
+            {
+                "speciality": vacancy.speciality,
+                "https_path": vacancy.https_path,
+                "salary_str": vacancy.salary_str,
+                "requirements": vacancy.requirements,
+            }
+        )
 
         # Записываем обновлённый список обратно в файл.
         self.__write_file(vacancy_list)
 
-        # Дозаписываем новую вакансию.
-        # self.__append_to_file(vacancy)
-
-
     def delete_data_from_file(self, vacancy: Vacancy) -> None:
-        """ Метод удаления данных (вакансии) из файла. """
+        """Метод удаления данных (вакансии) из файла."""
 
         vacancy_list: list = self.get_data_from_file()
 
-        deleted_vacancy_list: list = [vac_dict for vac_dict in vacancy_list if vac_dict["speciality"] != vacancy.speciality]
+        deleted_vacancy_list: list = [
+            vac_dict for vac_dict in vacancy_list if vac_dict["speciality"] != vacancy.speciality
+        ]
 
         # Записываем обновлённый список обратно в файл.
         self.__write_file(deleted_vacancy_list)
-
-
-
 
 
 if __name__ == "__main__":
@@ -144,9 +134,6 @@ if __name__ == "__main__":
     jh.delete_data_from_file(vac_2)
     # jh.add_data_to_file(vac_3)
 
-
-
-
     # help(open)
     # ========= ===============================================================
     # Character Meaning
@@ -159,7 +146,6 @@ if __name__ == "__main__":
     # 't'       text mode (default)
     # '+'       open a disk file for updating (reading and writing)
     # ========= ===============================================================
-
 
     # json_str = """
     # {
@@ -183,14 +169,12 @@ if __name__ == "__main__":
     #     print(man_dict["phone"])
     #     man_dict["country"] = "Russia"
 
-
     # Преобразование Python-объектов в строку формата json.
     # dump   - выгружаем из Python
     #     s  - в строковый вариант
     # json_str_2 = json.dumps(data)
     # print("json_str_2:   ", json_str_2)
     # print("type(json_str_2)", type(json_str_2))
-
 
     # # Запись данных типа str в файл (формата json).
     # with open("../data/test_example.json", "w", encoding="utf-8") as file:
@@ -220,25 +204,24 @@ if __name__ == "__main__":
     # with open("../data/test_example.json", "w", encoding="utf-8") as file:
     #     json.dump(321.999, file)
 
-
     # Чтение json-файла - т.е. преобразование содержимого файла в   Python - объект.
     # with open("../data/test_example.json", "r", encoding="utf-8") as j_file:
     #     python_data = json.load(j_file)
     # print("python_data:   ", python_data)
-    # # {"people": [{"name": "Ivan", "phone": "777", "country": "Russia"}, {"name": "Irina", "phone": "963", "country": "Russia"}]}
+    # # {"people": [{"name": "Ivan", "phone": "777", "country": "Russia"},
+    # # {"name": "Irina", "phone": "963", "country": "Russia"}]}
     #
     # print("type(python_data)):   ", type(python_data))
-
 
     # # Преобразование строкового формата в Python-типы:
     # python_data = json.loads(python_data_from_file___str)
     # print("python_data:   ", python_data)
-    # # {'people': [{'name': 'Ivan', 'phone': '777', 'country': 'Russia'}, {'name': 'Irina', 'phone': '963', 'country': 'Russia'}]}
+    # # {'people': [{'name': 'Ivan', 'phone': '777', 'country': 'Russia'},
+    # # {'name': 'Irina', 'phone': '963', 'country': 'Russia'}]}
     # print("type(python_data):   ", type(python_data))       # <class 'dict'>
     # for man_dict in python_data["people"]:
     #     man_dict["phone"] = f"+({man_dict["phone"]})"       # 'phone': '777'   --->     'phone': '+(777)'
     # print("python_data:   ", python_data)
-
 
     # # Запись полученного Python-объекта (python_data) обратно в файл (формата json):
     # with open("../data/test_example.json", "w", encoding="utf-8") as file:
@@ -246,9 +229,6 @@ if __name__ == "__main__":
 
     # directory = os.getcwd().split("/")[-1]
     # print("os.getcwd() -> directory:  ", directory)
-
-
-
 
     # @staticmethod
     # def create_file(file_name: str) -> None:
